8ee8e135175abb94d17433d53c7c52db041ef10e
K14aNB <kirnb@protonmail.com>
2024-06-08 21:58:10 +0530

    Added RF Model and Features

--- Notebooks/Titanic-Kaggle-Competition-Submission.py
+++ Notebooks/Titanic-Kaggle-Competition-Submission.py
@@ -13,19 +13,19 @@
 # ---
 
 # %% [markdown]
-# # **Titanic Kaggle Competition Submission**
+# # **Titanic Kaggle Competition - Submission**
 
 # %% [markdown]
 # **Check and install the dependencies**
 
 # %%
-# !curl -sSL https://raw.githubusercontent.com/K14aNB/Titanic-Kaggle-Competition/main/requirements.txt
+# !curl -sSL https://raw.githubusercontent.com/K14aNB/titanic-kaggle-competition/main/requirements.txt
 
 # %%
 # Run this command in terminal before running this notebook as .py script
 # Installs dependencies from requirements.txt present in the repo
 # %%capture
-# !pip install -r https://raw.githubusercontent.com/K14aNB/Titanic-Kaggle-Competition/main/requirements.txt
+# !pip install -r https://raw.githubusercontent.com/K14aNB/titanic-kaggle-competition/main/requirements.txt
 
 # %% [markdown]
 # **Import the libraries**
@@ -41,13 +41,22 @@ import os
 
 # %%
 # Setup Environment(Downloading data and setting output formats specified in config.yaml)
-result_path=env_setup.setup(repo_name='Titanic-Kaggle-Competition',nb_name='Titanic-Kaggle-Competition-Submission')
+result_path=env_setup.setup(repo_name='titanic-kaggle-competition',nb_name='Titanic-Kaggle-Competition-Submission')
 
 # %% [markdown]
-# **Get Artifact from MLFlow**
+# **Retrieve Runs from MLFlow**
 
 # %%
-mlflow.search_runs(experiment_names=['Titanic-Kaggle-Competition'])
+runs=mlflow.search_runs(experiment_names=['Titanic-Kaggle-Competition'])
+
+# %%
+runs.head()
+
+# %% [markdown]
+# **Get Model from run_id**
+
+# %%
+logged_model=f'runs:/{runs.loc[0,"run_id"]}/{runs.loc[0,"tags.mlflow.runName"]}'
 
 # %% [markdown]
 # **Read the data**
@@ -56,13 +65,34 @@ mlflow.search_runs(experiment_names=['Titanic-Kaggle-Competition'])
 X_test=pd.read_csv(os.path.join(result_path,'test.csv'))
 
 # %%
-logged_model='runs:/195fb6f7f28a4b20b128e005a5617119/Logistic Regression_CV'
+X_test.head()
+
+# %%
+X_test.info()
+
+# %% [markdown]
+# **Load the model from MLFlow**
 
 # %%
 # Load model as a PyFuncModel
-loaded_model=mlflow.pyfunc.load_model(logged_model)
+loaded_model=mlflow.sklearn.load_model(logged_model)
+
+# %% [markdown]
+# **Model Predictions**
 
 # %%
-loaded_model(X_test)
+predictions=loaded_model.predict(X_test)
+
+# %%
+output=pd.DataFrame({'PassengerId':X_test['PassengerId'],'Survived':predictions})
+
+# %%
+output.head()
+
+# %%
+output.to_csv('submission.csv',index=False)
+
+# %%
+# !kaggle competitions submit -f submission.csv -m 'DecisionTree_v0.8' titanic
 
 # %%
